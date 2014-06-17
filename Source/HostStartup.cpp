06cb88a9b4ee71eb4eb271e293c3c44d416ad580
Oli Larkin <olilarkin@googlemail.com>
2014-06-17 13:43:55 +0100

    hoststartup.h

--- Source/HostStartup.cpp
+++ Source/HostStartup.cpp
@@ -1,77 +1,63 @@
-#include "../JuceLibraryCode/JuceHeader.h"
-#include "MainHostWindow.h"
-#include "InternalFilters.h"
+#include "HostStartup.h"
 
 #if ! (JUCE_PLUGINHOST_VST || JUCE_PLUGINHOST_VST3 || JUCE_PLUGINHOST_AU)
 #error "If you're building the audio plugin host, you probably want to enable VST and/or AU support"
 #endif
 
-class pMixApp  : public JUCEApplication
+pMixApp::pMixApp() {}
+
+void pMixApp::initialise (const String& commandLine)
 {
-public:
-  pMixApp() {}
+  splash = new SplashScreen ("pMix",
+                             ImageCache::getFromMemory (BinaryData::pMix_about_png, BinaryData::pMix_about_pngSize),
+                             true);
+  
+  splash->deleteAfterDelay (RelativeTime::seconds (4), false);
+  
+  // initialise our settings file..
 
-  void initialise (const String& commandLine)
+  PropertiesFile::Options options;
+  options.applicationName     = "pMix";
+  options.filenameSuffix      = "settings";
+  options.osxLibrarySubFolder = "Preferences";
+
+  appProperties = new ApplicationProperties();
+  appProperties->setStorageParameters (options);
+
+  LookAndFeel::setDefaultLookAndFeel (&lookAndFeel);
+
+  mainWindow = new MainHostWindow();
+  mainWindow->setUsingNativeTitleBar (true);
+
+  commandManager.registerAllCommandsForTarget (this);
+  //commandManager.registerAllCommandsForTarget (mainWindow);
+
+  mainWindow->menuItemsChanged();
+
+  if (commandLine.isNotEmpty() && ! commandLine.trimStart().startsWith ("-") && mainWindow->getGraphEditor() != nullptr)
   {
-    splash = new SplashScreen ("pMix",
-                               ImageCache::getFromMemory (BinaryData::pMix_about_png, BinaryData::pMix_about_pngSize),
-                               true);
-    
-    splash->deleteAfterDelay (RelativeTime::seconds (4), false);
-    
-    // initialise our settings file..
-
-    PropertiesFile::Options options;
-    options.applicationName     = "pMix";
-    options.filenameSuffix      = "settings";
-    options.osxLibrarySubFolder = "Preferences";
-
-    appProperties = new ApplicationProperties();
-    appProperties->setStorageParameters (options);
-
-    LookAndFeel::setDefaultLookAndFeel (&lookAndFeel);
-
-    mainWindow = new MainHostWindow();
-    mainWindow->setUsingNativeTitleBar (true);
-
-    commandManager.registerAllCommandsForTarget (this);
-    //commandManager.registerAllCommandsForTarget (mainWindow);
-
-    mainWindow->menuItemsChanged();
-
-    if (commandLine.isNotEmpty() && ! commandLine.trimStart().startsWith ("-") && mainWindow->getGraphEditor() != nullptr)
-    {
-      mainWindow->getGraphEditor()->graph.loadFrom (File::getCurrentWorkingDirectory().getChildFile (commandLine), true);
-    }
+    mainWindow->getGraphEditor()->graph.loadFrom (File::getCurrentWorkingDirectory().getChildFile (commandLine), true);
   }
+}
 
-  void shutdown()
-  {
-    mainWindow = nullptr;
-    appProperties = nullptr;
-    LookAndFeel::setDefaultLookAndFeel (nullptr);
-  }
+void pMixApp::shutdown()
+{
+  mainWindow = nullptr;
+  appProperties = nullptr;
+  LookAndFeel::setDefaultLookAndFeel (nullptr);
+}
 
-  void systemRequestedQuit()
-  {
-    if (mainWindow != nullptr)
-      mainWindow->tryToQuitApplication();
-    else
-      JUCEApplicationBase::quit();
-  }
+void pMixApp::systemRequestedQuit()
+{
+  if (mainWindow != nullptr)
+    mainWindow->tryToQuitApplication();
+  else
+    JUCEApplicationBase::quit();
+}
 
-  const String getApplicationName()       { return "pMix"; }
-  const String getApplicationVersion()    { return ProjectInfo::versionString; }
-  bool moreThanOneInstanceAllowed()       { return true; }
-
-  ApplicationCommandManager commandManager;
-  ScopedPointer<ApplicationProperties> appProperties;
-  LookAndFeel_V3 lookAndFeel;
-
-private:
-  ScopedPointer<MainHostWindow> mainWindow;
-  SplashScreen* splash;
-};
+const String pMixApp::getApplicationName()       { return "pMix"; }
+const String pMixApp::getApplicationVersion()    { return ProjectInfo::versionString; }
+bool pMixApp::moreThanOneInstanceAllowed()       { return false; }
 
 static pMixApp& getApp()                      { return *dynamic_cast<pMixApp*>(JUCEApplication::getInstance()); }
 ApplicationCommandManager& getCommandManager()      { return getApp().commandManager; }
