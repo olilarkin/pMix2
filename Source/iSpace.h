d48242b6af66489213902a58122262d43047978c
Oli Larkin <olilarkin@googlemail.com>
2014-06-17 13:59:29 +0100

    iSpace.cpp

--- Source/iSpace.h
+++ Source/iSpace.h
@@ -2,6 +2,8 @@
 #ifndef _ISPACE_H_
 #define _ISPACE_H_
 
+#include "../JuceLibraryCode/JuceHeader.h"
+
 //class iSpaceComponent  : public Component
 //                       , private OpenGLRenderer
 //{
@@ -55,119 +57,52 @@
 //  OpenGLContext openGLContext;
 //};
 
+class iSpaceLabel : public Label
+{
+public:
+  iSpaceLabel(const String& labelText);
+};
+
+class MovePresetAction : public UndoableAction
+{
+public:
+  MovePresetAction (Component* iSpace, const String& componentID, Rectangle<int> startBounds, Rectangle<int> endBounds) noexcept;
+  bool perform();
+  bool undo();
+  int getSizeInUnits();
+  
+private:
+  Component* iSpace;
+  String componentID;
+  Rectangle<int> startBounds;
+  Rectangle<int> endBounds;
+  JUCE_DECLARE_NON_COPYABLE (MovePresetAction)
+};
+
+class iSpacePreset : public Component
+{
+private:
+  ComponentDragger myDragger;
+  ComponentBoundsConstrainer boundsConstrainer;
+  UndoManager& undoManager;
+  Rectangle<int> startBounds;
+  Rectangle<int> endBounds;
+  iSpaceLabel* label;
+    
+public:
+  iSpacePreset(UndoManager& undoManager, String& initalLabel);
+  ~iSpacePreset ();
+  void resized ();
+  void mouseDown (const MouseEvent& e);
+  void mouseDrag (const MouseEvent& e);
+  void mouseUp (const MouseEvent& e);
+  void paint (Graphics& g);
+};
+
 class iSpaceComponent  : public Component
                        , public LassoSource<Component*>  
 {
 private:
-  
-  class iSpaceLabel : public Label
-  {
-  public:
-    iSpaceLabel(const String& labelText) : Label(String::empty, labelText)
-    {
-    }
-  };
-  
-  class iSpacePreset  : public Component
-  {
-  private:
-    ComponentDragger myDragger;
-    ComponentBoundsConstrainer boundsConstrainer;
-    UndoManager& undoManager;
-    Rectangle<int> startBounds;
-    Rectangle<int> endBounds;
-    iSpaceLabel* label;
-    
-    class MovePresetAction  : public UndoableAction
-    {
-    public:
-      MovePresetAction (Component* iSpace, const String& componentID, Rectangle<int> startBounds, Rectangle<int> endBounds) noexcept
-      : iSpace(iSpace)
-      , componentID(componentID)
-      , startBounds(startBounds)
-      , endBounds(endBounds)
-      {
-      }
-      
-      bool perform()
-      {
-        iSpace->findChildWithID(componentID)->setBounds(endBounds);
-        return true;
-      }
-      
-      bool undo()
-      {      
-        iSpace->findChildWithID(componentID)->setBounds(startBounds);
-        return true;
-      }
-      
-      int getSizeInUnits()
-      {
-        return (int) sizeof (*this); //xxx should be more accurate
-      }
-      
-    private:
-      Component* iSpace;
-      String componentID;
-      Rectangle<int> startBounds;
-      Rectangle<int> endBounds;
-      JUCE_DECLARE_NON_COPYABLE (MovePresetAction)
-    };
-    
-  public:
-    iSpacePreset(UndoManager& undoManager, String& initalLabel)
-    : undoManager (undoManager)
-    {
-      addAndMakeVisible (label = new iSpaceLabel (initalLabel));
-      label->setFont (Font (13.00f));
-      label->setJustificationType (Justification::centred);
-      label->setEditable (false, true, false);
-      label->setColour (Label::textColourId, Colours::white);
-      label->setColour (TextEditor::textColourId, Colours::white);
-      label->setColour (TextEditor::backgroundColourId, Colour (0x00000000));
-    }
-
-    ~iSpacePreset ()
-    {
-      deleteAllChildren();
-    }
-
-    void resized ()
-    {
-      int radius = getWidth()/2;
-      boundsConstrainer.setMinimumOnscreenAmounts(radius,radius,radius,radius);
-      label->centreWithSize(getWidth()-5, 20);
-    }
-
-    void mouseDown (const MouseEvent& e)
-    {
-      myDragger.startDraggingComponent (this, e);
-      toFront (true);
-      startBounds = getBounds();
-    }
-
-    void mouseDrag (const MouseEvent& e)
-    {
-      myDragger.dragComponent (this, e, &boundsConstrainer);
-    }
-
-    void mouseUp (const MouseEvent& e)
-    {
-      endBounds = getBounds();
-      
-      undoManager.beginNewTransaction();
-      undoManager.perform(new MovePresetAction(getParentComponent(), getComponentID(), startBounds, endBounds), "change preset bounds");
-    }
-
-    void paint (Graphics& g)
-    {
-      g.setColour(Colours::red);
-      g.fillEllipse (0, 0, getWidth(), getHeight());
-      //g.setColour(Colours::white);
-      //g.drawFittedText(getComponentID(), 0, 0, getWidth(), getHeight(), Justification::centred, 1);
-    }
-  };
-  
   //TooltipWindow tooltipWindow;
   UndoManager& undoManager;
   Random mRand;
@@ -175,78 +110,15 @@ private:
   LassoComponent<Component*> lassoComp;
   
 public:
-  iSpaceComponent (UndoManager& undoManager)
-  : undoManager (undoManager)
-  , mRand(Time::currentTimeMillis())
-  {
-    for(int i = 0; i<10; i++)
-    {
-      String lab = String("preset") + String(i);
-      iSpacePreset* const comp = new iSpacePreset(undoManager, lab);
-      comp->setComponentID(lab);
-      addAndMakeVisible (comp);
-    }
-  }
-
-  ~iSpaceComponent ()
-  {
-    deleteAllChildren();
-  }
-
-  void resized ()
-  {
-    for (int i = 0; i < getNumChildComponents(); ++i)
-    {
-      iSpacePreset* const comp = dynamic_cast <iSpacePreset*> (getChildComponent(i));
-
-      float r = 50. + (50. * mRand.nextFloat());
-      float x = getWidth() * mRand.nextFloat();
-      float y = getHeight() * mRand.nextFloat();
-      comp->setBounds(x, y, r, r);
-    }
-  }
-
-  void paint (Graphics& g)
-  {
-    g.fillAll (Colours::white);
-  }
-  
-  void mouseDown (const MouseEvent& e)
-  {
-    addChildComponent (lassoComp);
-    lassoComp.beginLasso (e, this);
-  }
-  
-  void mouseDrag (const MouseEvent& e)
-  {
-    lassoComp.toFront (false);
-    lassoComp.dragLasso (e);
-  }
-  
-  void mouseUp (const MouseEvent& e)
-  {
-    lassoComp.endLasso();
-    removeChildComponent (&lassoComp);
-  }
-  
-  //LassoSource
-  void findLassoItemsInArea (Array <Component*>& results, const Rectangle<int>& area)
-  {
-    //  const Rectangle<int> lasso (area - subCompHolder->getPosition());
-    //  
-    //  for (int i = 0; i < subCompHolder->getNumChildComponents(); ++i)
-    //  {
-    //    Component* c = subCompHolder->getChildComponent (i);
-    //    
-    //    if (c->getBounds().intersects (lasso))
-    //      results.add (c);
-    //  }
-  }
-  
-  SelectedItemSet <Component*>& getLassoSelection()
-  {
-    return selectedItems;
-  }
+  iSpaceComponent (UndoManager& undoManager);
+  ~iSpaceComponent ();
+  void resized ();
+  void paint (Graphics& g);
+  void mouseDown (const MouseEvent& e);
+  void mouseDrag (const MouseEvent& e);
+  void mouseUp (const MouseEvent& e);
+  void findLassoItemsInArea (Array <Component*>& results, const Rectangle<int>& area);
+  SelectedItemSet <Component*>& getLassoSelection();
 };
 
 #endif//_ISPACE_H_ 
