39a4040429bd8b15f61f3e19d11d5c34ef76edf1
Oli Larkin <olilarkin@googlemail.com>
2014-06-17 15:04:54 +0100

    lookandfeel.cpp

---
+++ Source/LookAndFeel.cpp
@@ -0,0 +1,78 @@
+#include "LookAndFeel.h"
+
+pMixLookAndFeel::pMixLookAndFeel() {};
+
+void pMixLookAndFeel::drawPropertyComponentLabel (Graphics& g, int width, int height, PropertyComponent& component)
+{
+  g.setColour (Colours::black);
+  
+  if (! component.isEnabled())
+    g.setOpacity (0.6f);
+  
+  g.setFont (jmin (height, 24) * 0.65f);
+  
+  const Rectangle<int> r (getPropertyComponentContentPosition (component));
+  
+  g.drawText (component.getName(),
+              3, r.getY(), r.getX() - 5, r.getHeight(),
+              Justification::centredLeft, 2);
+}
+
+void pMixLookAndFeel::drawLinearSliderBackground (Graphics& g, int x, int y, int width, int height,
+                                                 float sliderPos, float minSliderPos, float maxSliderPos,
+                                                 const Slider::SliderStyle, Slider& slider)
+{
+  const float sliderRadius = (float) (getSliderThumbRadius (slider) - 2);
+  
+  const Colour trackColour (slider.findColour (Slider::trackColourId));
+  const Colour gradCol1 (trackColour.overlaidWith (Colour (slider.isEnabled() ? 0x13000000 : 0x09000000)));
+  const Colour gradCol2 (trackColour.overlaidWith (Colour (0x06000000)));
+  Path indent;
+  
+  if (slider.isHorizontal())
+  {
+    const float iy = y + height * 0.5f - sliderRadius * 0.5f;
+    
+    g.setColour (trackColour);
+    //g.setGradientFill (ColourGradient (gradCol1, 0.0f, iy,
+    //                                   gradCol2, 0.0f, iy + sliderRadius, false));
+    
+    indent.addRoundedRectangle (x - sliderRadius * 0.5f, iy, width + sliderRadius, sliderRadius, 5.0f);
+  }
+  else
+  {
+    const float ix = x + width * 0.5f - sliderRadius * 0.5f;
+    
+    
+    //g.setGradientFill (ColourGradient (gradCol1, ix, 0.0f,
+    //                                   gradCol2, ix + sliderRadius, 0.0f, false));
+    
+    indent.addRoundedRectangle (ix, y - sliderRadius * 0.5f, sliderRadius, height + sliderRadius, 5.0f);
+  }
+  
+  g.fillPath (indent);
+  
+  //g.setColour (trackColour.contrasting (0.5f));
+  //g.strokePath (indent, PathStrokeType (0.5f));
+}
+
+void pMixLookAndFeel::drawLasso (Graphics& g, Component& lassoComp)
+{    
+  g.fillAll (lassoComp.findColour (0x1000440 /*lassoFillColourId*/));
+  g.setColour (lassoComp.findColour (0x1000441 /*lassoOutlineColourId*/));
+  
+  Rectangle<int> bounds = lassoComp.getLocalBounds();
+  Path linePath;
+  linePath.addRectangle(bounds.getX(), bounds.getY(), bounds.getWidth(), bounds.getHeight());
+  
+  PathStrokeType stroke (2.5f);
+  float dashes[2] = { 4, 4 };
+  stroke.createDashedStroke(linePath, linePath, dashes, 2);
+  
+  g.fillPath (linePath);
+}
+
+Font pMixLookAndFeel::getPopupMenuFont()
+{
+  return Font (12.0f);
+}
--- Source/LookAndFeel.h
+++ Source/LookAndFeel.h
@@ -1,92 +1,22 @@
-#ifndef __JUCE_LookAndFeelCustom_JUCEHEADER__
-#define __JUCE_LookAndFeelCustom_JUCEHEADER__
 
-class LookAndFeelCustom : public LookAndFeel_V3
+#ifndef __pMixLookAndFeel_JUCEHEADER__
+#define __pMixLookAndFeel_JUCEHEADER__
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+class pMixLookAndFeel : public LookAndFeel_V3
 {
 public:
-
-  LookAndFeelCustom() {};
-  ~LookAndFeelCustom() {};
-
-  void drawPropertyComponentLabel (Graphics& g, int, int height, PropertyComponent& component)
-  {
-    g.setColour (Colours::black);
-    
-    if (! component.isEnabled())
-      g.setOpacity (0.6f);
-    
-    g.setFont (jmin (height, 24) * 0.65f);
-    
-    const Rectangle<int> r (getPropertyComponentContentPosition (component));
-    
-    g.drawText (component.getName(),
-                      3, r.getY(), r.getX() - 5, r.getHeight(),
-                      Justification::centredLeft, 2);
-  }
-  
+  pMixLookAndFeel();
+  void drawPropertyComponentLabel (Graphics& g, int width, int height, PropertyComponent& component);
   void drawLinearSliderBackground (Graphics& g, int x, int y, int width, int height,
-                              float /*sliderPos*/,
-                              float /*minSliderPos*/,
-                              float /*maxSliderPos*/,
-                              const Slider::SliderStyle /*style*/, Slider& slider)
-  {
-    const float sliderRadius = (float) (getSliderThumbRadius (slider) - 2);
-    
-    const Colour trackColour (slider.findColour (Slider::trackColourId));
-    const Colour gradCol1 (trackColour.overlaidWith (Colour (slider.isEnabled() ? 0x13000000 : 0x09000000)));
-    const Colour gradCol2 (trackColour.overlaidWith (Colour (0x06000000)));
-    Path indent;
-    
-    if (slider.isHorizontal())
-    {
-      const float iy = y + height * 0.5f - sliderRadius * 0.5f;
-      
-      g.setColour (trackColour);
-      //g.setGradientFill (ColourGradient (gradCol1, 0.0f, iy,
-      //                                   gradCol2, 0.0f, iy + sliderRadius, false));
-      
-      indent.addRoundedRectangle (x - sliderRadius * 0.5f, iy, width + sliderRadius, sliderRadius, 5.0f);
-    }
-    else
-    {
-      const float ix = x + width * 0.5f - sliderRadius * 0.5f;
-      
-      
-      //g.setGradientFill (ColourGradient (gradCol1, ix, 0.0f,
-      //                                   gradCol2, ix + sliderRadius, 0.0f, false));
-      
-      indent.addRoundedRectangle (ix, y - sliderRadius * 0.5f, sliderRadius, height + sliderRadius, 5.0f);
-    }
-    
-    g.fillPath (indent);
-    
-    //g.setColour (trackColour.contrasting (0.5f));
-    //g.strokePath (indent, PathStrokeType (0.5f));
-  }
-  
-  void drawLasso (Graphics& g, Component& lassoComp)
-  {    
-    g.fillAll (lassoComp.findColour (0x1000440 /*lassoFillColourId*/));
-    g.setColour (lassoComp.findColour (0x1000441 /*lassoOutlineColourId*/));
-    
-    Rectangle<int> bounds = lassoComp.getLocalBounds();
-    Path linePath;
-    linePath.addRectangle(bounds.getX(), bounds.getY(), bounds.getWidth(), bounds.getHeight());
-    
-    PathStrokeType stroke (2.5f);
-    float dashes[2] = { 4, 4 };
-    stroke.createDashedStroke(linePath, linePath, dashes, 2);
-    
-    g.fillPath (linePath);
-  }
-  
-  Font getPopupMenuFont()
-  {
-    return Font (12.0f);
-  }
+                                   float sliderPos, float minSliderPos, float maxSliderPos,
+                                   const Slider::SliderStyle, Slider& slider);
+  void drawLasso (Graphics& g, Component& lassoComp);
+  Font getPopupMenuFont();
   
 private:
-  JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (LookAndFeelCustom);
+  JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (pMixLookAndFeel);
 };
 
-#endif   // __JUCE_LookAndFeelCustom_JUCEHEADER__
+#endif   // __pMixLookAndFeel_JUCEHEADER__
