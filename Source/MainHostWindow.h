34010ab10c2cc881e8c8584a31567da7331ce4d9
Oli Larkin <olilarkin@googlemail.com>
2015-01-28 17:47:16 +0100

    notes

--- Source/MainHostWindow.h
+++ Source/MainHostWindow.h
@@ -18,30 +18,35 @@ public:
   MainHostWindow(AudioDeviceManager* deviceManager);
   ~MainHostWindow();
 
+  //DocumentWindow
   void closeButtonPressed();
+  
+  //ChangeListener
   void changeListenerCallback (ChangeBroadcaster*);
 
+  //FileDragAndDropTarget
   bool isInterestedInFileDrag (const StringArray& files);
   void fileDragEnter (const StringArray& files, int, int);
   void fileDragMove (const StringArray& files, int, int);
   void fileDragExit (const StringArray& files);
   void filesDropped (const StringArray& files, int, int);
 
+  //MenuBarModel
   StringArray getMenuBarNames();
   PopupMenu getMenuForIndex (int topLevelMenuIndex, const String& menuName);
   void menuItemSelected (int menuItemID, int topLevelMenuIndex);
+  
+  //ApplicationCommandTarget
   ApplicationCommandTarget* getNextCommandTarget();
   void getAllCommands (Array <CommandID>& commands);
   void getCommandInfo (CommandID commandID, ApplicationCommandInfo& result);
   bool perform (const InvocationInfo& info);
 
+  //Unique
   bool tryToQuitApplication();
-
   void createPlugin (const PluginDescription* desc, int x, int y);
-
   void addPluginsToMenu (PopupMenu& m) const;
   const PluginDescription* getChosenType (const int menuID) const;
-
   MainComponent* getMainComponent() const;
 
 private:
