1973dc22f9e0748617d649d05facf2bf5c67415c
Oli Larkin <olilarkin@googlemail.com>
2015-03-11 21:55:22 +0000

    rejig generic editor classes

--- Source/pMixGenericEditor.cpp
+++ Source/pMixGenericEditor.cpp
@@ -9,110 +9,88 @@
 
 #include "pMixGenericEditor.h"
 
-class PMixProcessorParameterPropertyComp   : public PropertyComponent,
-private AudioProcessorListener,
-private Timer
+
+PMixParamSlider::PMixParamSlider (AudioProcessor& p, int paramIndex)
+: owner (p)
+, index (paramIndex)
 {
-public:
-  PMixProcessorParameterPropertyComp (const String& name, AudioProcessor& p, int paramIndex)
-  : PropertyComponent (name)
-  , owner (p)
-  , index (paramIndex)
-  , paramHasChanged (false)
-  , slider (p, paramIndex)
+  const int steps = owner.getParameterNumSteps (index);
+  
+  if (steps > 1 && steps < 0x7fffffff)
+    setRange (0.0, 1.0, 1.0 / (steps - 1.0));
+  else
+    setRange (0.0, 1.0);
+  
+  setSliderStyle (Slider::LinearHorizontal);
+  setTextBoxStyle (Slider::NoTextBox, false, 0, 0);
+  //setTextBoxIsEditable (true);
+  setPopupMenuEnabled(true);
+  setScrollWheelEnabled (true);
+}
+
+void PMixParamSlider::valueChanged()
+{
+  const float newVal = (float) getValue();
+  
+  if (owner.getParameter (index) != newVal)
   {
-    startTimer (100);
-    setPreferredHeight(16);
-    //slider.setPopupDisplayEnabled(true, 0);
-    addAndMakeVisible (slider);
-    owner.addListener (this);
+    owner.setParameterNotifyingHost (index, newVal);
+    updateText();
   }
+}
   
-  ~PMixProcessorParameterPropertyComp()
+String PMixParamSlider::getTextFromValue (double value)
+{
+  return owner.getParameterText (index) + " " + owner.getParameterLabel (index).trimEnd();
+}
+
+PMixProcessorParameterPropertyComp::PMixProcessorParameterPropertyComp (const String& name, AudioProcessor& p, int paramIndex)
+: PropertyComponent (name)
+, owner (p)
+, index (paramIndex)
+, paramHasChanged (false)
+, slider (p, paramIndex)
+{
+  startTimer (100);
+  setPreferredHeight(16);
+  //slider.setPopupDisplayEnabled(true, 0);
+  addAndMakeVisible (slider);
+  owner.addListener (this);
+}
+  
+PMixProcessorParameterPropertyComp::~PMixProcessorParameterPropertyComp()
+{
+  owner.removeListener (this);
+}
+  
+void PMixProcessorParameterPropertyComp::refresh()
+{
+  paramHasChanged = false;
+  
+  if (slider.getThumbBeingDragged() < 0)
+    slider.setValue (owner.getParameter (index), dontSendNotification);
+  
+  slider.updateText();
+}
+
+void PMixProcessorParameterPropertyComp::audioProcessorParameterChanged (AudioProcessor*, int parameterIndex, float)
+{
+  if (parameterIndex == index)
+    paramHasChanged = true;
+}
+
+void PMixProcessorParameterPropertyComp::timerCallback()
+{
+  if (paramHasChanged)
   {
-    owner.removeListener (this);
+    refresh();
+    startTimerHz (50);
   }
-  
-  void refresh() override
+  else
   {
-    paramHasChanged = false;
-    
-    if (slider.getThumbBeingDragged() < 0)
-      slider.setValue (owner.getParameter (index), dontSendNotification);
-    
-    slider.updateText();
+    startTimer (jmin (1000 / 4, getTimerInterval() + 10));
   }
-  
-  void audioProcessorChanged (AudioProcessor*) override  {}
-  
-  void audioProcessorParameterChanged (AudioProcessor*, int parameterIndex, float) override
-  {
-    if (parameterIndex == index)
-      paramHasChanged = true;
-  }
-  
-  void timerCallback() override
-  {
-    if (paramHasChanged)
-    {
-      refresh();
-      startTimerHz (50);
-    }
-    else
-    {
-      startTimer (jmin (1000 / 4, getTimerInterval() + 10));
-    }
-  }
-  
-private:
-  class PMixParamSlider  : public Slider
-  {
-  public:
-    PMixParamSlider (AudioProcessor& p, int paramIndex)  : owner (p), index (paramIndex)
-    {
-      const int steps = owner.getParameterNumSteps (index);
-      
-      if (steps > 1 && steps < 0x7fffffff)
-        setRange (0.0, 1.0, 1.0 / (steps - 1.0));
-      else
-        setRange (0.0, 1.0);
-      
-      setSliderStyle (Slider::LinearHorizontal);
-      setTextBoxStyle (Slider::NoTextBox, false, 0, 0);
-      //setTextBoxIsEditable (true);
-      setScrollWheelEnabled (true);
-    }
-    
-    void valueChanged() override
-    {
-      const float newVal = (float) getValue();
-      
-      if (owner.getParameter (index) != newVal)
-      {
-        owner.setParameterNotifyingHost (index, newVal);
-        updateText();
-      }
-    }
-    
-    String getTextFromValue (double value) override
-    {
-      return owner.getParameterText (index) + " " + owner.getParameterLabel (index).trimEnd();
-    }
-    
-  private:
-    AudioProcessor& owner;
-    const int index;
-    
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (PMixParamSlider)
-  };
-  
-  AudioProcessor& owner;
-  const int index;
-  bool volatile paramHasChanged;
-  PMixParamSlider slider;
-  
-  JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (PMixProcessorParameterPropertyComp)
-};
+}
 
 PMixGenericAudioProcessorEditor::PMixGenericAudioProcessorEditor (AudioProcessor* const p)
 : AudioProcessorEditor (p)
--- Source/pMixGenericEditor.h
+++ Source/pMixGenericEditor.h
@@ -12,6 +12,45 @@
 
 #include "JuceHeader.h"
 
+class PMixParamSlider  : public Slider
+{
+public:
+  PMixParamSlider (AudioProcessor& p, int paramIndex);
+  
+  void valueChanged() override;
+  String getTextFromValue (double value) override;
+  
+private:
+  AudioProcessor& owner;
+  const int index;
+  
+  JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (PMixParamSlider)
+};
+
+class PMixProcessorParameterPropertyComp : public PropertyComponent
+                                         , private AudioProcessorListener
+                                         , private Timer
+{
+public:
+  PMixProcessorParameterPropertyComp (const String& name, AudioProcessor& p, int paramIndex);
+  ~PMixProcessorParameterPropertyComp();
+  
+  void refresh() override;
+  
+  void audioProcessorChanged (AudioProcessor*) override  {}
+  
+  void audioProcessorParameterChanged (AudioProcessor*, int parameterIndex, float);
+  void timerCallback() override;
+
+private:
+  AudioProcessor& owner;
+  const int index;
+  bool volatile paramHasChanged;
+  PMixParamSlider slider;
+  
+  JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (PMixProcessorParameterPropertyComp)
+};
+
 class PMixGenericAudioProcessorEditor : public AudioProcessorEditor
 {
 public:
