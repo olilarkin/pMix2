bd51ac51657e4ba9a48e7fcda864e76664947b5b
Oli Larkin <olilarkin@googlemail.com>
2015-03-14 13:51:38 +0000

    plugin window allways on top

--- Source/pMixPluginWindow.cpp
+++ Source/pMixPluginWindow.cpp
@@ -20,7 +20,7 @@ type (t)
 {
   setSize (400, 300);
   setUsingNativeTitleBar(true);
-  //setAlwaysOnTop(true);
+  setAlwaysOnTop(true);
   //setDropShadowEnabled(false);
   setContentOwned (pluginEditor, true);
 
