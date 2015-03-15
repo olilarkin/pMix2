1d314af2f585be1910da7060fa19e7700415b0a3
Oli Larkin <olilarkin@googlemail.com>
2015-03-15 21:12:08 +0000

    add JUCE Library code

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -39,8 +39,8 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "pMix";
-    const char* const  versionString  = "1.0.0";
-    const int          versionNumber  = 0x10000;
+    const char* const  versionString  = "0.0.1";
+    const int          versionNumber  = 0x1;
 }
 #endif
 
