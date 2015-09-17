9f918249b3d59292061ed8ba92395382a655cb1f
Oli Larkin <olilarkin@googlemail.com>
2015-09-17 12:20:25 +0100

    updated windows project and jucer module with new module paths

--- JuceLibraryCode/modules/juce_gui_extra/juce_gui_extra.h
+++ JuceLibraryCode/modules/juce_gui_extra/juce_gui_extra.h
@@ -1,13 +1,5 @@
 // This is an auto-generated file to redirect any included
 // module headers to the correct external folder.
 
-#if defined (JUCER_XCODE_MAC_F6D2F4CF)
- #include "../../../../../juce/modules/juce_gui_extra/juce_gui_extra.h"
-#elif defined (JUCER_LINUX_MAKE_7346DA2A)
- #include "../../../../../juce/modules/juce_gui_extra/juce_gui_extra.h"
-#elif defined (JUCER_VS2013_78A5020)
- #include "../../../../../juce/juce_gui_extra/juce_gui_extra.h"
-#else
- #error "This file is designed to be used in an Introjucer-generated project!"
-#endif
+#include "../../../../../juce/modules/juce_gui_extra/juce_gui_extra.h"
 
