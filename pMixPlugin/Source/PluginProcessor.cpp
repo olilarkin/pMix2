af0986f018eb97a3f0d7d79e26366da04284b347
Oli Larkin <olilarkin@googlemail.com>
2017-08-15 02:20:09 +0100

    tidy up processor source code

--- pMixPlugin/Source/PluginProcessor.cpp
+++ pMixPlugin/Source/PluginProcessor.cpp
@@ -10,106 +10,6 @@ PMixPluginAudioProcessor::~PMixPluginAudioProcessor()
 {
 }
 
-const String PMixPluginAudioProcessor::getName() const
-{
-  return JucePlugin_Name;
-}
-
-int PMixPluginAudioProcessor::getNumParameters()
-{
-  return 0;
-}
-
-float PMixPluginAudioProcessor::getParameter (int index)
-{
-  return 0.0f;
-}
-
-void PMixPluginAudioProcessor::setParameter (int index, float newValue)
-{
-}
-
-const String PMixPluginAudioProcessor::getParameterName (int index)
-{
-  return String();
-}
-
-const String PMixPluginAudioProcessor::getParameterText (int index)
-{
-  return String();
-}
-
-const String PMixPluginAudioProcessor::getInputChannelName (int channelIndex) const
-{
-  return String (channelIndex + 1);
-}
-
-const String PMixPluginAudioProcessor::getOutputChannelName (int channelIndex) const
-{
-  return String (channelIndex + 1);
-}
-
-bool PMixPluginAudioProcessor::isInputChannelStereoPair (int index) const
-{
-  return true;
-}
-
-bool PMixPluginAudioProcessor::isOutputChannelStereoPair (int index) const
-{
-  return true;
-}
-
-bool PMixPluginAudioProcessor::acceptsMidi() const
-{
- #if JucePlugin_WantsMidiInput
-  return true;
- #else
-  return false;
- #endif
-}
-
-bool PMixPluginAudioProcessor::producesMidi() const
-{
- #if JucePlugin_ProducesMidiOutput
-  return true;
- #else
-  return false;
- #endif
-}
-
-bool PMixPluginAudioProcessor::silenceInProducesSilenceOut() const
-{
-  return false;
-}
-
-double PMixPluginAudioProcessor::getTailLengthSeconds() const
-{
-  return 0.0;
-}
-
-int PMixPluginAudioProcessor::getNumPrograms()
-{
-  return 1;
-}
-
-int PMixPluginAudioProcessor::getCurrentProgram()
-{
-  return 0;
-}
-
-void PMixPluginAudioProcessor::setCurrentProgram (int index)
-{
-}
-
-const String PMixPluginAudioProcessor::getProgramName (int index)
-{
-  return String();
-}
-
-void PMixPluginAudioProcessor::changeProgramName (int index, const String& newName)
-{
-}
-
 void PMixPluginAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
 {
   audioEngine.getGraph().setPlayConfigDetails (getTotalNumInputChannels(), getTotalNumOutputChannels(), sampleRate, samplesPerBlock);
@@ -126,16 +26,6 @@ void PMixPluginAudioProcessor::processBlock (AudioSampleBuffer& buffer, MidiBuff
   audioEngine.getGraph().processBlock (buffer, midiMessages);
 }
 
-bool PMixPluginAudioProcessor::hasEditor() const
-{
-  return true; // (change this to false if you choose to not supply an editor)
-}
-
-AudioProcessorEditor* PMixPluginAudioProcessor::createEditor()
-{
-  return new PMixPluginAudioProcessorEditor (*this);
-}
-
 void PMixPluginAudioProcessor::getStateInformation (MemoryBlock& destData)
 {
   ScopedPointer<XmlElement> xml = audioEngine.getDoc().createXml();
@@ -150,6 +40,11 @@ void PMixPluginAudioProcessor::setStateInformation (const void* data, int sizeIn
   {
     audioEngine.getDoc().restoreFromXml(*xmlState);
   }
+}
+
+AudioProcessorEditor* PMixPluginAudioProcessor::createEditor()
+{
+  return new PMixPluginAudioProcessorEditor (*this);
 }
 
 AudioProcessor* JUCE_CALLTYPE createPluginFilter()
--- pMixPlugin/Source/PluginProcessor.h
+++ pMixPlugin/Source/PluginProcessor.h
@@ -1,5 +1,4 @@
-#ifndef PLUGINPROCESSOR_H_INCLUDED
-#define PLUGINPROCESSOR_H_INCLUDED
+#pragma once
 
 #include "../JuceLibraryCode/JuceHeader.h"
 #include "pMixAudioEngine.h"
@@ -10,6 +9,17 @@ public:
 
   PMixPluginAudioProcessor();
   ~PMixPluginAudioProcessor();
+  
+  const String getName() const override { return JucePlugin_Name; }
+  bool acceptsMidi() const override { return true; }
+  bool producesMidi() const override { return true; }
+  double getTailLengthSeconds() const override { return 0.0; }
+  int getNumPrograms() override { return 1; }
+  int getCurrentProgram() override { return 0; }
+  void setCurrentProgram (int index) override {}
+  const String getProgramName (int index) override { return String(); }
+  void changeProgramName (int index, const String& newName) override {}
+  bool hasEditor() const override { return true; }
 
   void prepareToPlay (double sampleRate, int samplesPerBlock) override;
   void releaseResources() override;
@@ -17,32 +27,10 @@ public:
   void processBlock (AudioSampleBuffer&, MidiBuffer&) override;
 
   AudioProcessorEditor* createEditor() override;
-  bool hasEditor() const override;
 
-  const String getName() const override;
-
-  int getNumParameters() override;
-  float getParameter (int index) override;
-  void setParameter (int index, float newValue) override;
-
-  const String getParameterName (int index) override;
-  const String getParameterText (int index) override;
-
-  const String getInputChannelName (int channelIndex) const override;
-  const String getOutputChannelName (int channelIndex) const override;
-  bool isInputChannelStereoPair (int index) const override;
-  bool isOutputChannelStereoPair (int index) const override;
-
-  bool acceptsMidi() const override;
-  bool producesMidi() const override;
-  bool silenceInProducesSilenceOut() const override;
-  double getTailLengthSeconds() const override;
-
-  int getNumPrograms() override;
-  int getCurrentProgram() override;
-  void setCurrentProgram (int index) override;
-  const String getProgramName (int index) override;
-  void changeProgramName (int index, const String& newName) override;
+//  int getNumParameters() override;
+//  float getParameter (int index) override;
+//  void setParameter (int index, float newValue) override;
 
   void getStateInformation (MemoryBlock& destData) override;
   void setStateInformation (const void* data, int sizeInBytes) override;
@@ -54,7 +42,3 @@ private:
 
   JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (PMixPluginAudioProcessor)
 };
-
-
-
-#endif  // PLUGINPROCESSOR_H_INCLUDED
